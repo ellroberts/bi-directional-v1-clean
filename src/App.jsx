b25ad4cb7acf822d7bff267b791d12545a733bb0
ellroberts <elliot.roberts@gmail.com>
2025-05-29 00:06:32 +0100

    first test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="text-center mt-10 text-2xl text-blue-600">
-      Hello from Vite + React + Tailwind CSS!
+      I'm gradually learning this
     </div>
   );
 }
