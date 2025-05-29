f59653859b52783c1a6b700d680e990db2e02a35
ellroberts <elliot.roberts@gmail.com>
2025-05-29 09:54:24 +0100

    tst

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="text-center mt-10 text-2xl text-blue-600">
-      I'm gradually learning this and that and other stuff too! nice
+      I'm gradually learning this and that and other asasstuff too! nice
     </div>
   );
 }
